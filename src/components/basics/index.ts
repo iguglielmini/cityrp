16bccdef777e911a02cb076dc16a42cd789fc516
italo.guglielmini <italo.guglielmini@proluno.com.br>
2023-08-17 19:11:27 -0300

    feat: ajustes de mocks

--- src/components/basics/index.ts
+++ src/components/basics/index.ts
@@ -1,4 +1,3 @@
 export { Background } from "./Background";
 export { Button } from "./Button";
-export { Heading } from "./Heading";
 export { Avatar } from "./Avatar";
---
+++ src/mocks/listTicket.mock.ts
@@ -0,0 +1,22 @@
+const ListaTicketMocks = [
+  {
+    id: "1",
+    title: "Cheats",
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+  },
+  {
+    id: "2",
+    title: "Doação",
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+  },
+  {
+    id: "1",
+    title: "Uso de cheats",
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+  },
+];
+
+export { ListaTicketMocks };
